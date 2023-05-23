cc6c572da8a481777db07f09bb18cb4d5aa0d1d5
HeyKev-Cmd <ke012685@gmail.com>
2023-05-23 16:18:49 -0400

    add git ignore

--- kevin_web/settings.py
+++ kevin_web/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'kevin_web_app',
     'django.contrib.staticfiles',
+    'rest_framework'
     
 ]
 
--- kevin_web_app/urls.py
+++ kevin_web_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("",views.index,name="index"),
+    path("",views.IndexView.as_view(),name="index"),
+    path("api/v1/info",views.View_API.as_view())
 ]
