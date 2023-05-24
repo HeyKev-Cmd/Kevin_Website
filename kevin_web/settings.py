d57432a2079f3251012a42cb05039156d6a98685
HeyKev-Cmd <ke012685@gmail.com>
2023-05-23 22:15:29 -0400

    for  vercel

--- kevin_web/settings.py
+++ kevin_web/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY=config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG=config('DEBUG',cast=bool)
+DEBUG=False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
