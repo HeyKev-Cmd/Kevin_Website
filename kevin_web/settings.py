3c6f1b0843c58546b6abc7ad2f7d81847e6fc794
HeyKev-Cmd <ke012685@gmail.com>
2023-06-02 22:07:19 -0400

    update

--- kevin_web/settings.py
+++ kevin_web/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+
 import mimetypes
 mimetypes.add_type("text/css", ".css", True)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
